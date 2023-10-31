374956fb6fd7c229f55bce9e73f0f5cfd685b2df
joseph uscuvilca mendoza <145615032+joseph-uscuvicla-mendoza@users.noreply.github.com>
2023-10-31 15:55:18 -0500

    Formulario en python de operaciones matemáticas

---
+++ index.py
@@ -0,0 +1,10 @@
+numero1 = float(input("Ingrese el primer numero: "))
+numero2 = float(input("Ingrese el segundo numero: "))
+suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+print("La suma de los numeros es:", suma)
+print("La resta de los numeros es:", resta)
+print("La multiplicacion de los numeros es", multiplicacion)
+print("La division de los numeros es:", division ) 
